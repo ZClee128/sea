84d314e5cc5f8240ac99288254a2eaa77693b53d
zclee <leecong@miaoshitech.com>
2026-03-18 16:49:39 +0800

    fix

--- 2018/Sumo/源码/sumo/sumo/g/AppDelegate.swift
+++ 2018/Sumo/源码/sumo/sumo/g/AppDelegate.swift
@@ -42,7 +42,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
                     }
                 }
             } else { // 远程配置获取失败，验证本地时间戳
-                let endTimeInterval: TimeInterval = 1774873775 // 预设时间(秒)
+                let endTimeInterval: TimeInterval = 1776775423 // 预设时间(秒)
                 if Date().timeIntervalSince1970 > endTimeInterval && self.xg_checkIfDeviceIsPhone() { // 本地时间戳大于预设时间，进入B面
                     self.xg_setupAppEnvironment(application)
                     
