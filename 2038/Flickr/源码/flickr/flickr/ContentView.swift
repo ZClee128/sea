97b27ea747e7db2c841551fe6783bac1c73f9570
zclee <leecong@miaoshitech.com>
2026-04-01 14:44:21 +0800

    fix

---
+++ 2038/Flickr/源码/flickr/flickr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  flickr
+//
+//  Created by zclee on 2026/4/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2038/Flickr/源码/flickr/flickr/flickrApp.swift
@@ -0,0 +1,17 @@
+//
+//  flickrApp.swift
+//  flickr
+//
+//  Created by zclee on 2026/4/1.
+//
+
+import SwiftUI
+
+@main
+struct flickrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
